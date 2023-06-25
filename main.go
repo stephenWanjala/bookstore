97f33d56f65a734c3acf2c9a1472dfd7f80ef38b
Wanjala Stephen <stephenwanjala145@gmail.com>
2023-06-25 15:05:22 +0300

    use environment variables

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/stephenWanjala/bookstore/models"
 	"github.com/stephenWanjala/bookstore/router"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	dsn := "root:wanjala@tcp(localhost:3306)/bookstore?charset=utf8mb4&parseTime=True&loc=Local"
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Read database connection details from environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
